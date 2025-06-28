d560ff1da0ee1ef0db3984910d15878a78368467
groovygnome <jrp810@proton.me>
2025-06-27 20:08:26 -0400

    make some components

---
+++ src/Foods.jsx
@@ -0,0 +1,9 @@
+export function Foods() {
+  return (
+    <>
+      <h1>My favorite food used to be Chicken Parm</h1>
+      <h2>But now, I have grown, and my favorite food is Oreos</h2>
+    </>
+  )
+
+}
